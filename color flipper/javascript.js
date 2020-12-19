44f044082bb9d100add7c93ca79b187a69f5da96
lorenzofj <lorenzofran1@gmail.com>
2020-12-19 17:29:36 -0300

    color-flipper

---
+++ color flipper/javascript.js
@@ -0,0 +1,17 @@
+const hexadecimal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', function(){
+    let hexColor = "#";
+    for(let i=0; i<6; i++){
+        hexColor += hexadecimal[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hexadecimal.length);
+}
