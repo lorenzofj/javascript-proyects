4511b91c1e72b33def0d4202166eda920d122f54
lorenzofj <lorenzofran1@gmail.com>
2020-12-27 13:58:35 -0300

    minimum exercise

---
+++ minimun/script.js
@@ -0,0 +1,10 @@
+function minimum (first, second){
+    if(first > second){
+        console.log("The minimum number is " + second);
+    }
+    else{
+        console.log("The minimum number is " + first);
+    }
+}
+
+minimum(7, 17);
